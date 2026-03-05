034740e57a3d3e7f0d4ddaedb2708facc5fec600
Raul Vinícius <165701907+raultuee@users.noreply.github.com>
2026-03-05 12:41:29 -0300

    Desativando o redirecionamento

--- src/auth/PrivateRoute.tsx
+++ src/auth/PrivateRoute.tsx
@@ -1,11 +1,9 @@
 // src/auth/ProtectedRoute.tsx
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from './AuthContext';
+import { Outlet } from 'react-router-dom';
 
 const ProtectedRoute: React.FC = () => {
-  const { token } = useAuth();
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
